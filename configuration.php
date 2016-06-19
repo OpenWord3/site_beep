ca131dc4555c83c4e613fe939b2f715e197e2856
steephen king <steephen king>
2016-06-19 22:30:47 +0200

    19/06 22:30

--- configuration.php
+++ configuration.php
@@ -9,15 +9,11 @@
 	include("./modeles/modele_switch.php");
 	include("./modeles/modele_numero_entrant.php");
 	include("./modeles/modele_call_center.php");
-<<<<<<< HEAD
-	include("./modeles/modele_connexion_bdd.php");
 	include("./modeles/modele_user.php");
 	include("./modeles/modele_groupe.php");
 	include("./modeles/modele_conference.php");
 	include("./function_check_nums.php");
-=======
 	include("./modeles/modele_droit.php");
->>>>>>> 3ad2868b9b84085e956c732b38fcc980eac763a7
 
 	//Constante de l'index
 	define("INDEX","index.php");
