c092102d1253731b8d5dd54bf5ba0ea9e84a1253
steephen king <steephen king>
2016-06-20 23:05:24 +0200

    dd

--- controleurs/controleur_edit_conference.php
+++ controleurs/controleur_edit_conference.php
@@ -54,7 +54,7 @@ else{
 			$msg = "<h3 style='color:green'>L'option talk only et music à bien été modifier pour la conférence 790</h3>";
 		}
 		else if(($opt_talk ==1) && ($opt_music == 0)){
-			exec('sudo /var/script_beep/chg_option.pl'.' 6');
+			exec('sudo /var/script_beep/chg_option.pl 6');
 			$msg = "<h3 style='color:green'>L'option talk only à bien été modifier pour la conférence 790</h3>";
 		}		
 		else if(($opt_music ==1) && ($opt_talk == 0)){
@@ -76,7 +76,8 @@ else{
 //		}
 		
 		if(($opt_music == 1) && ($opt_talk == 1)){
-			exec('sudo /var/script_beep/chng_option_conference.pl '.$num.' 3');
+			//exec('sudo /var/script_beep/chng_option_conference.pl '.$num.' 3');
+			exec('sudo /home/steephen/test/chng_option_conference.pl '.$num.' 3');
 			$msg = "<h3 style='color:green'>L'option talk only et music à bien été modifier pour la conférence $num</h3>";
 		}	
 		else if(($opt_talk ==1) && ($opt_music == 0)){
