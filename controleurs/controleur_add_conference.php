17a7c2f639f94330da0ae5e639661517d3dbf070
steephen king <steephen king>
2016-06-20 22:41:52 +0200

    22:41

--- controleurs/controleur_add_conference.php
+++ controleurs/controleur_add_conference.php
@@ -20,28 +20,25 @@
 	$nums_users = nums_users();
 	$nums_callcenters = nums_callcenters();
 	$nums_conferences= nums_conferences();
-	$check_num = check_nums($nums_users,$nums_callcenters,$nums_conferences,$num);
+	$check_num = check_nums($nums_users,$nums_callcenters,$nums_conferences,$num_conf);
 
 	
 	if($check_num == 0){
 		add_conf($num_conf,$mdp_conf,$opt_talk,$opt_music,$id_user[0]);
 		exec('sudo /var/script_beep/create_conference.pl '.$num_conf.' '.$mdp_conf);
 		
-		if(($opt_music == 1) && ($opt_talk == 1)){
+		if($opt_music == 1 && $opt_talk == 1){
+			echo "hellooo";
 			exec('sudo /var/script_beep/chng_option_conference.pl '.$num_conf.' 3');
-			$msg = "<h3 style='color:green'>La conférence $num à bien été modifier </h3>";
 		}	
 		else if(($opt_talk ==1) && ($opt_music == 0)){
 			exec('sudo /var/script_beep/chng_option_conference.pl '.$num_conf.' 2');
-			$msg = "<h3 style='color:green'>La conférence $num à bien été modifier</h3>";
 		}		
 		else if(($opt_music ==1) && ($opt_talk == 0)){
 			exec('sudo /var/script_beep/chng_option_conference.pl '.$num_conf.' 1');
-			$msg = "<h3 style='color:green'>La conférence $num à bien été modifier</h3>";
 		}
 		else if(($opt_talk ==0) && ($opt_music == 0)){
 			exec('sudo /var/script_beep/chng_option_conference.pl '.$num_conf.' 4');
-			$msg = "<h3 style='color:green'>La conférence $num à bien été modifier</h3>";
 		}
 		
 		$msg = "<h3 style='color:green'>Le numero de conference $num_conf à bien ete ajouter</h3>";
