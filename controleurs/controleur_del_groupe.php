cef1674d3672c5d41b5efbb74062d352e9746aee
steephen king <steephen king>
2016-06-23 18:43:16 +0200

    23/06 18:43

--- controleurs/controleur_del_groupe.php
+++ controleurs/controleur_del_groupe.php
@@ -4,6 +4,7 @@
 	$id_groupe_default = find_groupe_default();
 	$id_contexte = find_id_groupe($contexte);
 	$select_users_contexte = select_users_contexte($contexte);
+	$show_groupes = show_groupes();
 	$default = "default";
 	
 	move_users_groupe($id_groupe_default[0],$id_contexte[0]);
@@ -11,6 +12,10 @@
 		
 	while($donnees = $select_users_contexte->fetch()){
 		exec('sudo /var/script_beep/change_contexte.sh '.$donnees['login'].' '.$donnees['contexte'].' '.$default.' '.$donnees['num'].' '.$donnees['protocole'].' '.$donnees['user'].' '.$donnees['mail']);
+	}
+	
+	while($donnees = $show_groupes->fetch()){
+		exec('sudo /var/script_beep/del_include.sh '.$contexte.' '.$donnees['nom']);
 	}
 	
 	$msg = "<h3 style='color:red'>Le groupe $contexte à bien été supprimer</h3>";
