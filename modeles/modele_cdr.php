b4e63027c18b677112c8f33e59a9f1a143292c7a
steephen king <steephen king>
2016-06-29 01:52:23 +0200

    ndd

--- modeles/modele_cdr.php
+++ modeles/modele_cdr.php
@@ -3,7 +3,7 @@
 	function graph_day($login,$date_1,$date_2){
 		global $cdr;
 		
-		$req = $cdr->query("SELECT SUM(duration) FROM cdr WHERE src LIKE '$login' AND (start <= '$date_1') AND (start >= '$date_2') AND lastapp LIKE 'dial'");
+		$req = $cdr->query("SELECT SUM(duration) FROM cdr WHERE src LIKE '$login' AND (start <= '$date_1') AND (start >= '$date_2') AND (lastapp LIKE 'dial' OR lastapp LIKE 'MeetMe')");
 		$donnees = $req->fetch();
 		
 		return $donnees;	
