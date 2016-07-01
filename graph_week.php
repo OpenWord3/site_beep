2f3d917f09912453c32d732d7068e8b40dcf8aae
steephen king <steephen king>
2016-07-01 12:38:06 +0200

    12:38

--- graph_week.php
+++ graph_week.php
@@ -94,11 +94,11 @@
  $Test->drawBarGraph($DataSet->GetData(),$DataSet->GetDataDescription(),TRUE);  
   
  // Finish the graph  
- $login = $login . "_week.png";
+ $login_week = $login . "_week.png";
  $Test->setFontProperties("./chart/Fonts/tahoma.ttf",8);  
  $Test->drawLegend(300,150,$DataSet->GetDataDescription(),255,255,255);  
  $Test->setFontProperties("./chart/Fonts/tahoma.ttf",10);  
- $Test->Render("./users_graphs/$login.png");
+ $Test->Render("./users_graphs/$login_week.png");
  
  // $last_calls = last_calls($login);
  
