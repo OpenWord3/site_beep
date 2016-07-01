38aa86fdf3c4bcdf0bf3ebed24d8db6c3e574a20
steephen king <steephen king>
2016-07-01 12:03:33 +0200

    12:03

--- controleurs/controleur_show_user_page.php
+++ controleurs/controleur_show_user_page.php
@@ -38,8 +38,8 @@
  // Dataset definition   
  $DataSet = new pData;  
  // $DataSet->AddPoint(array($graph_yesterday_2[0]),"Serie3");  
- $DataSet->AddPoint(array($graph_day_outgoing[0]),"Serie2");  
- $DataSet->AddPoint(array($graph_day_incoming[0]),"Serie1");  
+ $DataSet->AddPoint(array(200),"Serie2");  
+ $DataSet->AddPoint(array(400),"Serie1");  
  $DataSet->AddAllSeries();  
  $DataSet->SetAbsciseLabelSerie();  
  $DataSet->SetYAxisName("Secondes");
