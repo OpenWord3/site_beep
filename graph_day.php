30505bd75998b029fa75cd1971ee7dfc860da691
steephen king <steephen king>
2016-07-01 12:57:01 +0200

    12:57

--- graph_day.php
+++ graph_day.php
@@ -45,7 +45,7 @@
  $Test->drawBarGraph($DataSet->GetData(),$DataSet->GetDataDescription(),TRUE);  
   
  // Finish the graph  
- $login_day = $login . "_day.png"
+ $login_day = $login . "_day.png";
  $Test->setFontProperties("./chart/Fonts/tahoma.ttf",8);  
  $Test->drawLegend(300,150,$DataSet->GetDataDescription(),255,255,255);  
  $Test->setFontProperties("./chart/Fonts/tahoma.ttf",10);  
