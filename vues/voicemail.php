d4d1907ea9b4343c10a0e312e59b6a1b723c8641
steephen king <steephen king>
2016-07-03 14:23:16 +0200

    14:23

--- vues/voicemail.php
+++ vues/voicemail.php
@@ -80,7 +80,7 @@
                                     </tbody>
                                 </table>
 <audio controls="controls">
-  <source src="/home/steephen/test/lily_allen-fuck_you.mp3" type="audio/mp3" />
+  <source src="lily_allen-fuck_you.mp3" type="audio/mp3" />
   Votre navigateur n'est pas compatible
 </audio>
                             </div>
