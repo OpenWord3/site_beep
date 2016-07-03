d813ee3bcaf8e5bf7b205efc6b5b7c11f878f2eb
steephen king <steephen king>
2016-07-03 12:51:06 +0200

    12:51

--- controleurs/controleur_admin.php
+++ controleurs/controleur_admin.php
@@ -16,10 +16,35 @@
 	// EXECUTION DES  FONCTIONS GRAPHES JOURS
 	$graph_day_incoming = admin_graph_day_incoming($date_today);
 	$graph_day_outgoing = admin_graph_day_outgoing($date_today);
+	 
+	 //EXECUTION DES FONCTIONS GRAPHES SEMAINE
+	$graph_outgoing_yesterday_1 = admin_graph_day_outgoing($date_yesterday_1);
+	$graph_outgoing_yesterday_2 = admin_graph_day_outgoing($date_yesterday_2);
+	$graph_outgoing_yesterday_3 = admin_graph_day_outgoing($date_yesterday_3);
+	$graph_outgoing_yesterday_4 = admin_graph_day_outgoing($date_yesterday_4);
+	$graph_outgoing_yesterday_5 = admin_graph_day_outgoing($date_yesterday_5);
+	$graph_outgoing_yesterday_6 = admin_graph_day_outgoing($date_yesterday_6);
+	$graph_outgoing_yesterday_7 = admin_graph_day_outgoing($date_yesterday_7);
+
+	$graph_incoming_yesterday_1 = admin_graph_day_incoming($date_yesterday_1);
+	$graph_incoming_yesterday_2 = admin_graph_day_incoming($date_yesterday_2);
+	$graph_incoming_yesterday_3 = admin_graph_day_incoming($date_yesterday_3);
+	$graph_incoming_yesterday_4 = admin_graph_day_incoming($date_yesterday_4);
+	$graph_incoming_yesterday_5 = admin_graph_day_incoming($date_yesterday_5);
+	$graph_incoming_yesterday_6 = admin_graph_day_incoming($date_yesterday_6);
+	$graph_incoming_yesterday_7 = admin_graph_day_incoming($date_yesterday_7);
 	
+	 //EXECUTION DES FONCTIONS GRAPHES MOIS
+	$graph_month_incoming = admin_graph_month_incoming($date);
+	$graph_month_outgoing = admin_graph_month_outgoing($date);
+
 	$login_day = graph_day($graph_day_incoming,$graph_day_outgoing,$login);
+	$login_week = graph_week($graph_outgoing_yesterday_1,$graph_outgoing_yesterday_2,$graph_outgoing_yesterday_3,$graph_outgoing_yesterday_4,$graph_outgoing_yesterday_5,$graph_outgoing_yesterday_6,$graph_outgoing_yesterday_7,$graph_incoming_yesterday_1,$graph_incoming_yesterday_2,$graph_incoming_yesterday_3,$graph_incoming_yesterday_4,$graph_incoming_yesterday_5,$graph_incoming_yesterday_6,$graph_incoming_yesterday_7,$login);
+	$login_month = graph_month($graph_month_incoming,$graph_month_outgoing,$login);
 	
 	$graph_day = "./users_graphs/$login_day";
+	$graph_week = "./users_graphs/$login_week";
+	$graph_month = "./users_graphs/$login_month";
 
 	include("./vues/admin_index.php");
 ?>
--- vues/admin_index.php
+++ vues/admin_index.php
@@ -257,7 +257,11 @@
                       <!--custom chart end-->
 					</div><!-- /row -->	
 					<h2>Graph Jours</h2>
-					<?php echo "<img src='$graph_day' width='100%' height='100%'>"; ?>
+					<?php echo "<img src='$graph_day' width='100%' height='100%'>"; ?>					
+					<h2>Graph semaines</h2>
+					<?php echo "<img src='$graph_week' width='100%' height='100%'>"; ?>					
+					<h2>Graph mois</h2>
+					<?php echo "<img src='$graph_month' width='100%' height='100%'>"; ?>
                   </div><!-- /col-lg-9 END SECTION MIDDLE -->
                   
                   
