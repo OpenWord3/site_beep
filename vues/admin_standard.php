5a4bc6f0afc4da9cadd432e720a0b38315d3b630
MougninSLA <mougnin@intechinfo.fr>
2016-07-03 10:36:16 +0200

    super

--- vues/admin_standard.php
+++ vues/admin_standard.php
@@ -154,6 +154,7 @@
 					$end_minute=sprintf("%02d",$_POST['end_minute']);
 					
 					exec('sudo /var/script_beep/standard.sh '.$begin_heure.' '.$begin_minute.' '.$end_heure.' '.$end_minute.' '.$begin_jour.' '.$end_jour.' '.$user);
+					include("admin_standard.php");
 				}
 			?>
 		</section><! --/wrapper -->
