89846e705099a05e9e0a337eb89151e0e4ddaf6e
steephen king <steephen king>
2016-07-04 12:07:55 +0200

    12:07

--- controleurs/controleur_show_voicemail.php
+++ controleurs/controleur_show_voicemail.php
@@ -3,5 +3,7 @@
  $num = select_user($id_user);
  $num = $num['num'];
  
+ exec('sudo /var/script_beep/count_vocale  '.$num);
+ $count_vocale = fopen("/var/script_beep/count_vocale.txt");
 include("./vues/voicemail.php");
 ?>
--- vues/voicemail.php
+++ vues/voicemail.php
@@ -41,7 +41,7 @@
                             </div>
 						</div>
 					</div>
-					
+					<?php echo $count_vocale; ?>
 					<?php include("./assets/pages/footer.php"); ?>
 
 				</div>
