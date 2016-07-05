795a0e00a19c2e03fab7475a75e2377beda55b8b
steephen king <steephen king>
2016-07-05 23:59:44 +0200

    23:50

--- check_pseudo.php
+++ check_pseudo.php
@@ -1,4 +1,10 @@
 <?php	
+	try{
+		$bdd = new PDO("mysql:host=localhost;dbname=beep;charset=utf8", "root", "africainetfier", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	}catch(Exception $e){
+		die("ERREUR : ".$e->getMessage());
+	}
+	
 	global $bdd;
 	
 	$login = $_POST['login'];
